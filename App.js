1acbacfd3cd399d8fc6fa918eb8a6c2278d422e9
givenng6 <givenng6@gmail.com>
2023-04-23 17:29:15 +0200

    buggie sound

--- App.js
+++ App.js
@@ -1,54 +1,118 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
+import { Audio } from 'expo-av';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function App() {
-  const [{ x, y, z }, setData] = useState({
-    x: 0,
-    y: 0,
-    z: 0,
-  });
-  const [subscription, setSubscription] = useState(null);
 
-  const _slow = () => Accelerometer.setUpdateInterval(1000);
-  const _fast = () => Accelerometer.setUpdateInterval(16);
+    const [vertDirection, setVertDirection] = useState(0);
+    const [direction, setDirection] = useState('Forward');
+    const [prevVertDirection, setPrevVertDirection] = useState('Forward');
+    const [leftBlinker, setLeftBlinker] = useState('gray');
+    const [rightBlinker, setRightBlinker] = useState('gray');
+    const [sound, setSound] = useState();
+    let prev = 0;
 
-  const _subscribe = () => {
-    setSubscription(
-      Accelerometer.addListener(setData)
-    );
-  };
 
-  const _unsubscribe = () => {
-    subscription && subscription.remove();
-    setSubscription(null);
-  };
+    useEffect(() => {
+        return sound ? () => {sound.unloadAsync()}: undefined;
+    }, [sound]);
 
-  useEffect(() => {
-    _subscribe();
-    return () => _unsubscribe();
-  }, []);
+    async function playSound(currDirection) {
+        pauseSound();
+        if(currDirection == 'left'){
+            const { sound } = await Audio.Sound.createAsync(require('./sound/300.wav'));
+            setSound(sound);
+            await sound.playAsync();
+        }else{
+            const { sound } = await Audio.Sound.createAsync(require('./sound/500.wav'));
+            setSound(sound);
+            await sound.playAsync();
+        }
+    }
+
+    const pauseSound = async () => {
+        setSound(null);
+    };
+
+    const onChangeDirection = (currDirection, prevDirection)=>{
+        if(currDirection > 0.3){
+            setDirection('Turning Right');
+            if(prevDirection < 0.3){
+                playSound('right');
+            }
+            setLeftBlinker('gray');
+            setRightBlinker('red');
+            setTimeout(()=>{
+                setRightBlinker('gray');
+            }, 1700);
+        }else if(currDirection < -0.3){
+            setDirection('Turning Left');
+            if(prevDirection < -0.3){
+                console.log(`Left: ${currDirection} -> ${prevDirection}`)
+                playSound('left');
+            }
+            setRightBlinker('gray');
+            setLeftBlinker('red');
+            setTimeout(()=>{
+                setLeftBlinker('gray');
+            }, 1700);
+        }else{
+            pauseSound();
+            setDirection('Forward');
+            setLeftBlinker('gray');
+            setRightBlinker('gray');
+        }
+    }
+
+    const onChange = () => {
+        Accelerometer.addListener(data => {
+            onChangeDirection(data.y, prev);
+            setVertDirection(data.y);
+            setPrevVertDirection(data.y);
+            prev= data.y;
+        });
+    };
+
+    useEffect(() => {
+        onChange();
+    }, []);
+
 
   return (
-    <View style={{marginTop: 30}}>
-      <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-          <Text>{subscription ? 'On' : 'Off'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
-          <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
-          <Text>Fast</Text>
-        </TouchableOpacity>
-      </View>
+    <View style={styles.Main}>
+        <View style = {{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <MaterialCommunityIcons name="chevron-triple-left" size={100} color={leftBlinker} />
+        <View style = {{flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginHorizontal: 100}}>
+        <Text style = {styles.Dir}>{direction}</Text>
+        <Text style = {{color: 'white'}}>{vertDirection}</Text>
+        </View>
+        <MaterialCommunityIcons name="chevron-triple-right" size={100} color={rightBlinker} />
+        </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
-
+    Main:{
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        backgroundColor: 'black'
+    },
+    Dir:{
+        fontSize: 50,
+        fontWeight: '300',
+        color: 'white'
+    },
+    Indicator:{
+        width: 80,
+        height: 80,
+        borderColor: 'black',
+        backgroundColor: 'gray',
+        borderRadius: 100
+    }
 });
